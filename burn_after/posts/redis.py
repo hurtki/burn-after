a1d5f71eb358e46df6a5bd161ebc56fc59bd35f2
uno/hurtki <asboba2101@gmail.com>
2025-04-09 13:36:19 +0300

    to files to interact with cache

--- burn_after/posts/redis.py
+++ burn_after/posts/redis.py
@@ -1,60 +1,76 @@
-# сборник функций для работы с кешем 
-
-from django.core.cache import cache
+# redis.py
 from .models import Category, Post
 from django.conf import settings
 from .serializers import PostSerializer
+from .redis_helper import cache, cache_set_json, cache_get_json, cache_delete, get_zset_length
+from django.db.models import Count
 
-# функция для получения списка категорий из кеша
+# Функция для получения списка категорий из кеша
 def get_categories_from_cache():
-    categories = cache.get('categories_list')
+    categories = cache_get_json('categories_list')
     if not categories:
         categories = list(Category.objects.values_list('name', flat=True))
-        cache.set('categories_list', categories, timeout=settings.CATEGORIES_LIST_CACHE_SECONDS)  
+        cache_set_json('categories_list', categories, ex=settings.CATEGORIES_LIST_CACHE_SECONDS)
     return categories
-# функция для занесения категории в кеш
+
+
+# Функция для занесения категории в кеш
 def add_category_to_cache(new_category_name):
     categories = get_categories_from_cache()
     if new_category_name not in categories:
         categories.append(new_category_name)
-        cache.set('categories_list', categories, timeout=settings.CATEGORIES_LIST_CACHE_SECONDS)
+        cache_set_json('categories_list', categories, ex=settings.CATEGORIES_LIST_CACHE_SECONDS)
 
 
+# Функция для получения сериализованного поста из кеша
 def get_serialized_post_data_from_cache(ids):
-    # создаем дикт для хранения сериализированных данных постов 
+    # создаем дикт для хранения сериализованных данных постов
     cached_data = {}
-    # лист для хранения айдишников которые мы не найдем в кеше 
+    # лист для хранения айдишников, которые мы не найдем в кеше
     missing_ids = []
 
-    # проверяем каждый ключ в кеше 
+    # проверяем каждый ключ в кеше
     for post_id in ids:
-        data = cache.get(f'post:{post_id}')
-        # если есть данные то сохраняем в дикт если нету то в лист
+        data = cache_get_json(f'post:{post_id}')
         if data is not None:
             cached_data[post_id] = data
         else:
             missing_ids.append(post_id)
 
-    # теперь если есть айди которых не было  
+    # если есть айдишники, которых не было в кеше
     if missing_ids:
-        # получаем только их из базы данных 
+        # получаем только их из базы данных
         objects = Post.objects.filter(id__in=missing_ids)
-        # перебираем полученные объекты
         for obj in objects:
-            # сериализируем каждого из них 
             serialized_data = PostSerializer(obj).data
-            # записываем сериализированный пост в кеш
-            cache.set(f'post:{obj.id}', serialized_data)
-            # добавляем сериализированные данные в возвращаемый дикт
+            cache_set_json(f'post:{obj.id}', serialized_data, ex=settings.POST_CACHE_SECONDS)
             cached_data[obj.id] = serialized_data
 
-    # собираем 
+    # собираем результат
     result = [cached_data[obj_id] for obj_id in ids]
     
     return result
 
+
+# Получаем посты из Zset
 def get_posts_for_page(zset_key, start, end, sort):
     if "-" in sort:
         return cache.zrevrange(zset_key, start, end)
     else:
         return cache.zrange(zset_key, start, end)
+
+
+# Проверяем, есть ли список постов в кеше, если нет — добавляем его
+def ensure_zset_cached(zset_key: str, category: Category, sort: str, is_exploded: bool) -> None:
+    if not cache.zcard(zset_key):
+        posts = Post.objects.filter(category=category, is_exploded=is_exploded).annotate(like_count=Count('likes'))
+
+        for post in posts:
+            score = post.created_at.timestamp() if sort == "created_at" else post.like_count
+            cache.zadd(zset_key, {str(post.id): score})
+            serialized_post = PostSerializer(post).data
+            cache_set_json(f'post:{post.id}', serialized_post, ex=settings.POST_CACHE_SECONDS)
+
+
+def get_length_of_zset(zset_key: str) -> int:
+    return get_zset_length(zset_key)
