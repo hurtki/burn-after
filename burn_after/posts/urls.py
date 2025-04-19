32413731bec68fbf12213be259f3b978fc8badab
uno/hurtki <asboba2101@gmail.com>
2025-04-19 12:54:55 +0300

    add comments for urlpatterns

--- burn_after/posts/urls.py
+++ burn_after/posts/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     # api для получения постов на главной странице 
     path('', views.PostsAPIView.as_view()),
-    
+    # дальше будут апишки для какого-то взаимодействия с четким постом 
+    # первая для смены состояния лайка, только для авторизированных пользователей 
+    #  path('<int>/like', views.LikeAPIView.as_view())
 ]
